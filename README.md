08c245f67b38174c277927924bea429a4741da59
RishabhSaini <rishabhsaini01@gmail.com>
2023-09-23 14:27:56 -0400

    Add email address and required validation for UofT

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
 
 ## Activity 3
 ![Activity 3 Screenshot](./imgs/a3.png?raw=true "Activity 3 Screenshot")
+
+## Activity 4
+![Activity 4 Screenshot](./imgs/a4p1.png?raw=true "Activity 4 Screenshot")
+![Activity 4 Screenshot](./imgs/a4p2.png?raw=true "Activity 4 Screenshot")
+![Activity 4 Screenshot](./imgs/a4p3.png?raw=true "Activity 4 Screenshot")
+![Activity 4 Screenshot](./imgs/a4p4.png?raw=true "Activity 4 Screenshot")
--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -13,6 +13,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,5 +24,9 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name')
         session['name'] = form.name.data
+        email = form.email.data
+        session['email'] = email
+        if 'utoronto' in email:
+            session['uoft_email'] = email
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), uoft_email=session.get('uoft_email'))
